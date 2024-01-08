90bf622bf8db42d0415f1dc78440b7879ab9c5f3
Joseph <josephmacdonald4629@gmail.com>
2024-01-08 14:01:09 -0400

    show better way

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -13,6 +13,22 @@ namespace Structs
             Console.WriteLine("Struts");
             Console.WriteLine();
 
+            // no structs
+            // no classes (objects)
+
+            // player
+            int health = 100;
+            int shield = 100;
+            int score = 0;
+            int playerx = 10;
+            int playery = 10;
+
+            // 1000 enemys
+            int numEnemies = 1000;
+            int[] enemyX = new int[numEnemies];
+            int[] enemyY = new int[numEnemies];
+
+
             Console.WriteLine("Press any key to exit the program");
 
             Console.ReadKey(true);
